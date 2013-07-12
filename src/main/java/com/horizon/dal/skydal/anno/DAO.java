20cc91eab4a4f1d39c759f0a03ca18a367bf22ef
ianlin <ming.horizon@gmail.com>
2013-07-12 09:28:53 +0800

    初始化一些迁移的文件。

---
+++ src/main/java/com/horizon/dal/skydal/anno/DAO.java
@@ -0,0 +1,21 @@
+/**
+ * [Copyright]
+ * @author ianlin
+ * @date 8:31:27 PM
+ */
+package com.horizon.dal.skydal.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author ianlin
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface DAO {
+
+}
